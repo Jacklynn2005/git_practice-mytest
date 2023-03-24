f65eee0b7e7ab7df0c308efb16ff5abe7b5d26ee
ãrogue <shadow_rogue@qq.com>
2023-03-24 21:24:13 +0800

    add: main file

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * ****************************(C) COPYRIGHT 2023 Blue Bear****************************
+ * @file       main.cpp
+ * @brief      
+ * 
+ * @note       
+ * @history:
+ *   Version   Date            Author          Modification    Email
+ *   V1.0.0    Mar-24-2023     Tianyi Zhang    1. start        tz137@duke.edu/shadow_rogue@qq.com
+ * 
+ * @verbatim
+ * ==============================================================================
+ * 
+ * ==============================================================================
+ * @endverbatim
+ * ****************************(C) COPYRIGHT 2023 Blue Bear****************************
+ */
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello world!" << endl;
+    return 0;
+}
